883015b6751252bd90e887c4131c7fbae980bcab
MichaelChung123 <michael.chung1994@gmail.com>
2019-03-12 12:32:33 -0700

    Setting up twitter authentication

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import TwitterLogin from 'react-twitter-auth';
 
 import Tweets from "./Tweets";
 
@@ -12,7 +13,10 @@ class App extends Component {
 
     this.state = {
       tweets: [],
-      searchString: ""
+      searchString: "",
+      isAuthenticated: false,
+      user: null,
+      token: ''
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,7 +28,7 @@ class App extends Component {
       searchString: e.target.value
     });
   }
-  
+
   // Sends search string to server, and performs get request to obtain tweets from Twitter API on server end
   handleSubmit(e) {
     e.preventDefault();
@@ -48,12 +52,50 @@ class App extends Component {
     });
   }
 
+  onSuccess = (response) => {
+    const token = response.headers.get('x-auth-token');
+    response.json().then(user => {
+      if (token) {
+        this.setState({ isAuthenticated: true, user: user, token: token });
+      }
+    });
+  };
+
+  onFailed = (error) => {
+    alert(error);
+  };
+
+  logout = () => {
+    this.setState({ isAuthenticated: false, token: '', user: null })
+  };
+
   render() {
+
+    let content = !!this.state.isAuthenticated ?
+      (
+        <div>
+          <p>Authenticated</p>
+          <div>
+            {this.state.user.email}
+          </div>
+          <div>
+            <button onClick={this.logout} className="button" >
+              Log out
+          </button>
+          </div>
+        </div>
+      ) :
+      (
+        <TwitterLogin loginUrl="http://localhost:3000/api/v1/auth/twitter"
+          onFailure={this.onFailed} onSuccess={this.onSuccess}
+          requestTokenUrl="http://localhost:3000/api/v1/auth/twitter/reverse" />
+      );
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          {content}
           <p>
             Idyaflow Twitter Search
           </p>
